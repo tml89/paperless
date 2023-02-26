9ec82a26a5af93fd0c89aa2aac7d19ecf52f7f1d
tml89 <schmid.tim@pm.me>
2023-02-26 16:29:07 +0100

    Improve TextExtraction and file Date

--- Paperless.py
+++ Paperless.py
@@ -1,10 +1,13 @@
-from pypdf import PdfReader
+import tempfile
+import subprocess
 import re
 import os
 import ocrmypdf
 import dateparser
+import datetime
 import Logger
 import Settings
+import pikepdf
 from tendo import singleton
 
 # Check if another instance is already running
@@ -18,6 +21,39 @@ DATE_REGEX = re.compile(
     r"(\b|(?!=([_-])))([^\W\d_]{3,9} [0-9]{4})(\b|(?=([_-])))|"
     r"(\b|(?!=([_-])))(\b[0-9]{1,2}[ \.\/-][A-Z]{3}[ \.\/-][0-9]{4})(\b|(?=([_-])))",  # noqa: E501
 )
+
+def extract_text(pdf_file):
+    text = None
+    with tempfile.NamedTemporaryFile(
+        mode="w+",
+        dir="/var/tmp",
+    ) as tmp:
+        subprocess.run(
+            [
+                "pdftotext",
+                "-q",
+                "-layout",
+                "-enc",
+                "UTF-8",
+                pdf_file,
+                tmp.name,
+            ],
+        )
+        text = tmp.read()
+    return post_process_text(text)
+
+def post_process_text(text):
+    if not text:
+        return None
+
+    collapsed_spaces = re.sub(r"([^\S\r\n]+)", " ", text)
+    no_leading_whitespace = re.sub(r"([\n\r]+)([^\S\n\r]+)", "\\1", collapsed_spaces)
+    no_trailing_whitespace = re.sub(r"([^\S\n\r]+)$", "", no_leading_whitespace)
+
+    # TODO: this needs a rework
+    # replace \0 prevents issues with saving to postgres.
+    # text may contain \0 when this character is present in PDF files.
+    return no_trailing_whitespace.strip().replace("\0", " ")
 
 def matches(tags, text):  
     found = False  
@@ -47,8 +83,11 @@ def getDate(text,pdfFile):
 
     ##fallback to pdf creation date
     Logger.Log("Date in Text not found return creation date", Logger.LogLevel.Debug)
-    pdf_info = pdfFile.metadata
-    return pdf_info.creation_date.strftime("%Y-%m-%d")
+    pdf = pikepdf.open(pdfFile)
+    pdf_info = pdf.docinfo
+    creation_date_raw = pdf_info.get('/CreationDate')
+    creation_date = datetime.datetime.strptime(str(creation_date_raw), "D:%Y%m%d%H%M%S%z00'00'")
+    return creation_date.strftime("%Y-%m-%d")
     
 #----------------------
 
@@ -63,21 +102,13 @@ Logger.Log("---- Start ----", Logger.LogLevel.Info)
 for sourceFilename in os.listdir(Settings.NEXTCLOUDROOT + Settings.SOURCEDIR):
     sourceFilePath = os.path.join(Settings.NEXTCLOUDROOT + Settings.SOURCEDIR, sourceFilename)
     # checking if it is a file
-    if not os.path.isfile(sourceFilePath) or not sourceFilename.endswith(".pdf"):# or filename.startswith("["): 
+    if not os.path.isfile(sourceFilePath) or not sourceFilename.endswith(".pdf"):# or sourceFilename.startswith("["): 
         continue
     
     Logger.Log("Process File: " + sourceFilename, Logger.LogLevel.Info)
 
-    # Öffnen der PDF-Datei
-    reader = PdfReader(sourceFilePath)
-    number_of_pages = len(reader.pages)
-
-    Logger.Log("Number of Pages: " + str(number_of_pages), Logger.LogLevel.Debug)
-
     # Extrahieren des Texts aus der PDF-Datei
-    text = ''
-    for page in reader.pages:
-        text += page.extract_text()
+    text = extract_text(sourceFilePath)
 
     if text is None or len(text) < 50:
         try:
@@ -87,7 +118,7 @@ for sourceFilename in os.listdir(Settings.NEXTCLOUDROOT + Settings.SOURCEDIR):
         except: #ToDo: catch correct exceptions
             Logger.Log("Cannot OCR file", Logger.LogLevel.Error)
 
-    fileDate = getDate(text,reader)
+    fileDate = getDate(text,sourceFilePath)
     Logger.Log("File date: " + fileDate, Logger.LogLevel.Debug)
     
     Logger.Log("Find destdir", Logger.LogLevel.Debug)
@@ -102,7 +133,7 @@ for sourceFilename in os.listdir(Settings.NEXTCLOUDROOT + Settings.SOURCEDIR):
     Logger.Log("DestFilePath: " + destFilePath, Logger.LogLevel.Debug)
     Logger.Log("Move: " + sourceFilename + " to: " + destFilePath, Logger.LogLevel.Info)
     try:
-        pass#os.rename(sourceFilePath, destFilePath)
+        os.rename(sourceFilePath, destFilePath)
     except OSError as error:
         Logger.Log(str(error), Logger.LogLevel.Error)
 Logger.Log("---- End ----", Logger.LogLevel.Info)
